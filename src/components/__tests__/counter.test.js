862f42fdc5b087a9af0e639b755720debacd6476
erkina308 <erkinayna@hotmail.co.uk>
2024-05-02 12:46:38 +0100

    counter test and css made and updated

--- src/components/__tests__/counter.test.js
+++ src/components/__tests__/counter.test.js
@@ -1,16 +1,62 @@
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen, cleanup, fireEvent } from "@testing-library/react";
 import Counter from "../counter";
+
+afterEach(() => {
+  cleanup();
+});
 
 describe("", () => {
   test("should render Counter component", () => {
     //set up
     render(<Counter />);
 
-    //acted
-    const counterElement = screen.getByTestId("counter-1");
+    //declaration
+    const counterElement = screen.getByTestId("count");
 
     //assertions
     expect(counterElement).toBeInTheDocument();
-    expect(counterElement).toHaveTextContent("Hello World!");
+    expect(counterElement).toHaveTextContent(0);
+  });
+});
+
+describe("", () => {
+  test("When increment button is clicked; count goes up by 1", () => {
+    //set up
+    render(<Counter />);
+
+    //declaration
+    const counterElement = screen.getByTestId("count");
+    const incrementElement = screen.getByTestId("increment");
+
+    fireEvent.click(incrementElement);
+
+    //assertions
+    expect(counterElement).toBeInTheDocument();
+    expect(incrementElement).toBeInTheDocument();
+    expect(counterElement).toHaveTextContent(1);
+    fireEvent.click(incrementElement);
+    expect(counterElement).toHaveTextContent(2);
+  });
+});
+
+describe("", () => {
+  test("When decrement button is clicked; count goes down by 1", () => {
+    //set up
+    render(<Counter />);
+
+    //declaration
+    const counterElement = screen.getByTestId("count");
+    const incrementElement = screen.getByTestId("increment");
+    const decrementElement = screen.getByTestId("decrement");
+
+    //assertions
+    expect(counterElement).toBeInTheDocument();
+    expect(incrementElement).toBeInTheDocument();
+    expect(decrementElement).toBeInTheDocument();
+    expect(counterElement).toHaveTextContent(0);
+    fireEvent.click(incrementElement);
+    expect(counterElement).toHaveTextContent(1);
+    fireEvent.click(decrementElement);
+    expect(counterElement).toHaveTextContent(0);
   });
 });
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-function counter() {
-  return <div data-testid="counter-1">Hello World!</div>;
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount((prevCount) => prevCount - 1);
+    }
+  };
+
+  return (
+    <section id="counter_page">
+      <div className="counter_container">
+        <h1 data-testid="count">{count}</h1>
+        <div className="inc_dec_btns">
+          <button
+            data-testid="increment"
+            className="increment"
+            onClick={increment}
+          >
+            Increment
+          </button>
+          <button
+            data-testid="decrement"
+            className="decrement"
+            onClick={decrement}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default counter;
+export default Counter;
